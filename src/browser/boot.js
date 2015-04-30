c92acba078ea996a140a92cb50ac7152fe61b004
Brandon Fryslie <bfryslie@rallydev.com>
2015-04-30 01:32:16 -0600

    reorganized

--- src/browser/boot.js
+++ src/browser/boot.js
@@ -1,7 +1,6 @@
-var coffee = require('coffee-script');  // Module to create native browser window.
 var app = require('app');  // Module to control application life.
 var BrowserWindow = require('browser-window');  // Module to create native browser window.
-
+var path = require('path');
 // Report crashes to our server.
 require('crash-reporter').start();
 
@@ -15,18 +14,25 @@ app.on('window-all-closed', function() {
     app.quit();
 });
 
-// This method will be called when Electron has done everything
+
+// This method will be called when atom-shell has done everything
 // initialization and ready for creating browser windows.
 app.on('ready', function() {
+  var protocol = require('protocol');
+  protocol.registerProtocol('dash', function(request) {
+    var url = request.url.substr(7)
+    var search = url.indexOf('?');
+    if (search > -1) {
+      url = url.substr(0, search);
+    }
+    return new protocol.RequestFileJob(path.normalize(path.join(__dirname, '..', '..', url)));
+  });
+
   // Create the browser window.
   mainWindow = new BrowserWindow({width: 800, height: 600});
 
   // and load the index.html of the app.
-  mainWindow.loadUrl('file://' + __dirname + '/index.html');
-
-  // tracer = require('./tracer.coffee');
-  // for now, tracer is going to have to draw everything itself
-  // tracer.set_window(mainWindow)
+  mainWindow.loadUrl('file://' + __dirname + '/../../static/index.html');
 
   // Emitted when the window is closed.
   mainWindow.on('closed', function() {
